71c77d3ce3ba17790368ecfa7dd0cfe71292ee13
Dang Duy Linh <40199004+duylinhdang1998@users.noreply.github.com>
2019-02-12 18:10:38 +0700

    commit

--- index.js
+++ index.js
@@ -1,186 +1,88 @@
-require('dotenv').config()
-const express = require('express');
-const router = express();
-var bodyParser = require('body-parser');
-const http = require('http');
-var server = http.createServer(router);
+require('dotenv').config();
+const  express = require('express');
 
-const port = process.env.PORT || 3000;
-router.use(bodyParser.urlencoded({
-    extended: false,
-    limit: 2 * 1024 * 1024
-}));
-router.use(bodyParser.json({
-    limit: 2 * 1024 * 1024
-}));
+const  bodyParser = require('body-parser');
+const app= express()
+app.use(bodyParser.json());
+const PORT = process.env.PORT ||1137 
 
-console.log('dang duy linh')
-const APP_SECRET = process.env.MESSENGER_APP_SECRET
+const APP_SECRET = process.env.MESSENGER_APP_SECRET;
+const PAGE_ACCESS_TOKEN = process.env.MESSENGER_PAGE_ACCESS_TOKEN;
+const SEVER_URL= process.env.SEVER_URL;
 const VALIDATION_TOKEN = process.env.MESSENGER_VALIDATION_TOKEN
-const PAGE_ACCESS_TOKEN = process.env.MESSENGER_PAGE_ACCESS_TOKEN
-const SERVER_URL = process.env.SERVER_URL
-
-router.get('/', function (req, res) {
-    res.send("Home page. Server running...");
-});
-
-/*
- * Use your own validation token. Check that the token used in the Webhook 
- * setup is the same token used here.
- *
- */
-router.get('/webhook', function (req, res) {
-    if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === VALIDATION_TOKEN) {
-        console.log("Validating webhook");
-        res.status(200).send(req.query['hub.challenge']);
-    } else {
-        console.error("Failed validation. Make sure the validation tokens match.");
-        res.sendStatus(403);
-    }
-});
-
-/*
- * All callbacks for Messenger are POST-ed. They will be sent to the same
- * webhook. Be sure to subscribe your app to your page to receive callbacks
- * for your page. 
- * https://developers.facebook.com/docs/messenger-platform/product-overview/setup#subscribe_app
- *
- */
-router.post('/webhook', function (req, res) {
-    var data = req.body;
-
-    // Make sure this is a page subscription
-    if (data.object == 'page') {
-        // Iterate over each entry
-        // There may be multiple if batched
-        data.entry.forEach(function (pageEntry) {
-            var pageID = pageEntry.id;
-            var timeOfEvent = pageEntry.time;
-
-            // Iterate over each messaging event
-            pageEntry.messaging.forEach(function (messagingEvent) {
-                if (messagingEvent.message) {
-                    receivedMessage(messagingEvent);
-                }
-                else if (messagingEvent.delivery) {
-                    receivedDeliveryConfirmation(messagingEvent);
-                }
-                else {
-                    console.log("Webhook received unknown messagingEvent: ", messagingEvent);
-                }
-            });
-        });
-
-        // Assume all went well.
-        //
-        // You must send back a 200, within 20 seconds, to let us know you've 
-        // successfully received the callback. Otherwise, the request will time out.
-        res.sendStatus(200);
-    }
-});
 
 
+app.post('/webhook', (req, res) => {  
+ 
+  let body = req.body;
+  let webhook_event="";
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
 
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
 
-function receivedMessage(event) {
-    var senderID = event.sender.id;
-    var recipientID = event.recipient.id;
-    var timeOfMessage = event.timestamp;
-    var message = event.message;
-
-    console.log("Received message for user %d and page %d at %d with message:",
-        senderID, recipientID, timeOfMessage);
-    console.log(JSON.stringify(message));
-
-    var isEcho = message.is_echo;
-    var messageId = message.mid;
-    var appId = message.app_id;
-    var metadata = message.metadata;
-
-    // You may get a text or attachment but not both
-    var messageText = message.text;
-    var messageAttachments = message.attachments;
-    var quickReply = message.quick_reply;
-
-    if (isEcho) {
-        // Just logging message echoes to console
-        console.log("Received echo for message %s and app %d with metadata %s",
-            messageId, appId, metadata);
-        return;
-    } else if (quickReply) {
-        var quickReplyPayload = quickReply.payload;
-        console.log("Quick reply for message %s with payload %s",
-            messageId, quickReplyPayload);
-        sendTextMessage(senderID, "Quick reply tapped");
-        return;
-    }
-
-    if (messageText) {
-        // If we receive a text message, check to see if it matches any special
-        // keywords and send back the corresponding example. Otherwise, just echo
-        // the text we received.
-        switch (messageText) {
-            case 'image':
-                //sendImageMessage(senderID);
-                break;
-            default:
-                sendTextMessage(senderID, 'Echo:' + messageText);
-        }
-    } else if (messageAttachments) {
-        sendTextMessage(senderID, "Message with attachment received");
-    }
-}
-
-/*
- * Send a text message using the Send API.
- *
- */
-function sendTextMessage(recipientId, messageText) {
-    var messageData = {
-        recipient: {
-            id: recipientId
-        },
-        message: {
-            text: messageText,
-            metadata: "DEVELOPER_DEFINED_METADATA"
-        }
-    };
-
-    callSendAPI(messageData);
-}
-
-/*
- * Call the Send API. The message data goes in the body. If successful, we'll 
- * get the message id in a response 
- *
- */
-function callSendAPI(messageData) {
-    request({
-        uri: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: { access_token: PAGE_ACCESS_TOKEN },
-        method: 'POST',
-        json: messageData
-
-    }, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var recipientId = body.recipient_id;
-            var messageId = body.message_id;
-
-            if (messageId) {
-                console.log("Successfully sent message with id %s to recipient %s",
-                    messageId, recipientId);
-            } else {
-                console.log("Successfully called Send API for recipient %s",
-                    recipientId);
-            }
-        } else {
-            console.error(response.error);
-        }
+      // Gets the message. entry.messaging is an array, but 
+      // will only ever contain one message, so we get index 0
+       webhook_event = entry.messaging;
     });
-}
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function () {
-    var addr = server.address();
-    console.log("Chat server listening at", addr.address + ":" + addr.port);
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send(webhook_event);
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
 });
 
-module.exports = server;
+app.get('/webhook', (req, res) => {
+
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = VALIDATION_TOKEN;
+    
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+    
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+  
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      
+      // Responds with the challenge token from the request
+      res.status(200).send(challenge);
+    
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
+    }
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT , ()=>{
+  console.log("Sever listening  "+ PORT);
+})
